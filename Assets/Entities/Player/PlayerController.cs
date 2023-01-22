fa5a364f53651ccec5e71e4978639cdbd6dc08a2
Eduard Bryan P. Rivera <edubprivera@yahoo.com>
2023-01-22 13:22:17 +0800

    Add player walk controller

--- Assets/Entities/Player/PlayerController.cs
+++ Assets/Entities/Player/PlayerController.cs
@@ -10,25 +10,50 @@ public class PlayerController : MonoBehaviour
 
     Vector2 inputDirection;
     Rigidbody2D rigidBody;
+    Animator animator;
+    SpriteRenderer spriteRenderer;
     List<RaycastHit2D> collisionList = new List<RaycastHit2D>();
+    float horizontalDirection = 0;
+    float verticalDirection = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void FixedUpdate()
     {
         // Update player's movement
-        // Check player collision at given direction
-        int collisionCount = rigidBody.Cast(
-            inputDirection, contactFilter, collisionList, movementSpeed * Time.fixedDeltaTime
-        );
+        if (inputDirection != Vector2.zero) {
+            // Check player collision at given direction
+            int collisionCount = rigidBody.Cast(
+                inputDirection, contactFilter, collisionList, movementSpeed * Time.fixedDeltaTime
+            );
 
-        // Move player if no collisions detected at the given direction
-        if (collisionCount == 0) {
-            rigidBody.MovePosition(rigidBody.position + inputDirection * movementSpeed * Time.fixedDeltaTime);
+            // Move player if no collisions detected at the given direction
+            if (collisionCount == 0) {
+                rigidBody.MovePosition(rigidBody.position + inputDirection * movementSpeed * Time.fixedDeltaTime);
+
+                // Change direction
+                horizontalDirection = inputDirection.x;
+                verticalDirection = inputDirection.y;
+            }
+
+            animator.SetFloat("HorizontalDirection", horizontalDirection);
+            animator.SetFloat("VerticalDirection", verticalDirection);
+            animator.SetBool("isWalking", true);
+
+            // side facing
+            if (horizontalDirection < 0 && Mathf.Abs(horizontalDirection) >= Mathf.Abs(verticalDirection)) {
+                spriteRenderer.flipX = true;
+            } else {
+                spriteRenderer.flipX = false;
+            }
+        } else {
+            animator.SetBool("isWalking", false);
         }
     }
 
