9b4c205891cdaf28aa6bad4c8a4bff3f5f7d535e
Eduard Bryan P. Rivera <edubprivera@yahoo.com>
2023-01-25 15:43:56 +0800

    Refactor object movement to be grid based

--- Assets/Objects/Pushable Objects/PushableObjectController.cs
+++ Assets/Objects/Pushable Objects/PushableObjectController.cs
@@ -6,38 +6,44 @@ public class PushableObjectController : MonoBehaviour
 {
     public ContactFilter2D contactFilter;
     
-    float distanceToCover = 0;
     float speed = 3;
+    bool canMove = true;
     Vector2 moveDirection = new Vector2(0, 0);
+    Vector3 destination;
     Rigidbody2D rigidBody;
     List<RaycastHit2D> collidedObjects = new List<RaycastHit2D>();
 
     void Start() {
         rigidBody = GetComponent<Rigidbody2D>();
+        destination = transform.position;
     }
 
     void FixedUpdate() {
-        if (distanceToCover > 0) {
-            float distance = Mathf.Min(speed * Time.fixedDeltaTime, distanceToCover);
-            int collision = rigidBody.Cast(
-                moveDirection, contactFilter, collidedObjects, distance
-            );
-
-            if (collision == 0) {
-                rigidBody.MovePosition(rigidBody.position + moveDirection * distance);
-                distanceToCover -= distance;
-            }
+        if (transform.position != destination) {
+            transform.position = Vector2.MoveTowards(transform.position, destination, speed * Time.fixedDeltaTime);
+        } else {
+            canMove = true;
         }
     }
 
     public void MoveTo(Vector2 direction) {
-        // TODO: Clean up code
+        if (!canMove) return;
+
         if (direction.x != 0) {
-            moveDirection = new Vector2(direction.x > 0 ? 1 : -1, 0);
-            distanceToCover = 1;
+            SetDestination(new Vector2(direction.x, 0));
         } else if (direction.y != 0) {
-            moveDirection = new Vector2(0, direction.y > 0 ? 1 : -1);
-            distanceToCover = 1;
+            SetDestination(new Vector2(0, direction.y));
+        }
+    }
+
+    private void SetDestination(Vector2 direction) {
+        int collisionCount = rigidBody.Cast(
+            direction, contactFilter, collidedObjects, 1
+        );
+
+        if (collisionCount == 0) {
+            destination = transform.position + new Vector3(direction.x, direction.y).normalized;
+            canMove = false;
         }
     }
 }
