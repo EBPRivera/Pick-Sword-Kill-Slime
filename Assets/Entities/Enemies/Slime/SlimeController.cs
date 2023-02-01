3005593c82470a64fd380c80bfe59a5bfe183a55
Eduard Bryan P. Rivera <edubprivera@yahoo.com>
2023-02-01 12:55:58 +0800

    Add slime boilerplate
    Add slime animation

---
+++ Assets/Entities/Enemies/Slime/SlimeController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeController : MonoBehaviour
+{
+    public float health = 3f;
+    public bool isInvuln = false;
+
+    Animator animator;
+
+    void Start() {
+        animator = GetComponent<Animator>();
+    }
+
+    public void TakeDamage(float damage) {
+        health -= damage;
+        isInvuln = true;
+
+        if (health <= 0) {
+            animator.SetTrigger("Death");
+        } else {
+            animator.SetTrigger("Damaged");
+        }
+    }
+
+    public void SetNotInvuln() {
+        isInvuln = false;
+    }
+
+    public void Die() {
+        Destroy(gameObject);
+    }
+}
