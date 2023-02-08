053bad1608151a59494a2f943ad898011871b4f6
Eduard Bryan P. Rivera <edubprivera@yahoo.com>
2023-02-08 15:09:20 +0800

    Add health controller script

---
+++ Assets/Entities/HealthController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthController : MonoBehaviour, IDamageable
+{
+    public float Health { get => _health;
+        set {
+            _health = value;
+
+            if (_health < 0) {
+                animator.SetTrigger("Death");
+            } else {
+                animator.SetTrigger("Damaged");
+            }
+        }
+    }
+    public bool IsInvuln { get => _isInvuln;
+        set {
+            _isInvuln = value;
+        }
+    }
+    public float _health;
+    public bool _isInvuln;
+
+    Animator animator;
+    Rigidbody2D rb;
+
+    private void Start() {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    public void TakeDamage(float damage, Vector2 knockback)
+    {
+        if (!IsInvuln) {
+            Health -= damage;
+            rb.AddForce(knockback, ForceMode2D.Impulse);
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        if (!IsInvuln) {
+            Health -= damage;
+        }
+    }
+}
