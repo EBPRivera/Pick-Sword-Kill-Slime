3b3f87efa7fd53c0445687f20acf522a9e344ab9
Eduard Bryan P. Rivera <edubprivera@yahoo.com>
2023-03-04 18:00:14 +0800

    Add pickable object spawner

--- Assets/Scripts/PickableObject.cs
+++ Assets/Scripts/PickableObject.cs
@@ -13,4 +13,20 @@ public class PickableObject : MonoBehaviour {
             Destroy(gameObject);
         }
     }
+
+    public static PickableObject SpawnPickableObject(PickableSO pickableSO, Vector2 position, Transform parent = null) {
+        Transform pickableObjectTransform = Instantiate(pickableSO.prefab, parent);
+
+        PickableObject pickableObject = pickableObjectTransform.GetComponent<PickableObject>();
+        Vector2 colliderSize = pickableObject.GetComponent<Collider2D>().bounds.size;
+        Collider2D overlap = Physics2D.OverlapBox(position, colliderSize + colliderSize * 0.1f, 0);
+
+        if (overlap == null) {
+            pickableObjectTransform.position = position;
+            return pickableObject;
+        } else {
+            Destroy(pickableObjectTransform.gameObject);
+            return null;
+        }
+    }
 }
