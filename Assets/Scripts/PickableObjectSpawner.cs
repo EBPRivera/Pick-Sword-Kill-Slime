a71ac6bd476a186773b0947af14543fd3974629e
Eduard Bryan P. Rivera <edubprivera@yahoo.com>
2023-03-04 22:40:13 +0800

    Update pickable spawner properties as serialize field

--- Assets/Scripts/PickableObjectSpawner.cs
+++ Assets/Scripts/PickableObjectSpawner.cs
@@ -5,21 +5,21 @@ using UnityEngine;
 public class PickableObjectSpawner : MonoBehaviour {
     
     [SerializeField] private PickableListSO pickableListSO;
-    [SerializeField] private float width;
-    [SerializeField] private float height;
+    [SerializeField] private float width = 1;
+    [SerializeField] private float height = 1;
+    [SerializeField] private int objectCountMax = 4;
+    [SerializeField] private float spawnTimerMax = 3f;
 
     [Header("Gizmo")]
     [SerializeField] private Color gizmoColor;
 
-    private float pickableObjectSpawnTimer = 0f;
-    private float pickableObjectSpawnTimerMax = 3f;
-    private int pickableObjectCountMax = 4;
+    private float spawnTimer = 0f;
 
     private void Update() {
-        if (GameManager.Instance.IsPlayable() && transform.childCount <= pickableObjectCountMax) {
-            pickableObjectSpawnTimer += Time.deltaTime;
+        if (GameManager.Instance.IsPlayable() && transform.childCount <= objectCountMax) {
+            spawnTimer += Time.deltaTime;
 
-            if (pickableObjectSpawnTimer > pickableObjectSpawnTimerMax) {
+            if (spawnTimer > spawnTimerMax) {
                 // Add an object within the bounded box
                 PickableSO pickableObjectToSpawn = pickableListSO.pickableSOList[UnityEngine.Random.Range(0, pickableListSO.pickableSOList.Count)];
                 SpawnPickableObject(pickableObjectToSpawn);
@@ -29,12 +29,12 @@ public class PickableObjectSpawner : MonoBehaviour {
 
     private void SpawnPickableObject(PickableSO pickableObjectToSpawn) {
         Vector2 position = new Vector2(
-            UnityEngine.Random.Range(0, width) - width / 2,
-            UnityEngine.Random.Range(0, height) - height / 2
+            transform.position.x + UnityEngine.Random.Range(0, width) - width / 2,
+            transform.position.y + UnityEngine.Random.Range(0, height) - height / 2
         );
 
         if (PickableObject.SpawnPickableObject(pickableObjectToSpawn, position, transform) != null) {
-            pickableObjectSpawnTimer = 0f;
+            spawnTimer = 0f;
         }
     }
 
