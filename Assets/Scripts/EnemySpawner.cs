d1625590aa60bca7f62af2aa298e4e7621caf0c8
Eduard Bryan P. Rivera <edubprivera@yahoo.com>
2023-03-04 22:32:11 +0800

    Update enemy spawner as box

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -5,45 +5,41 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour {
 
     [SerializeField] private EntityListSO entityListSO;
-    [SerializeField] private float spawnRadius;
+    [SerializeField] private float width = 1;
+    [SerializeField] private float height = 1;
+    [SerializeField] private int enemyCountMax = 5;
+    [SerializeField] private float spawnTimerMax = 4f;
 
     [Header("Gizmos")]
     [SerializeField] private Color gizmoColor;
     
-    private int enemyCountMax = 5;
-    private float enemySpawnTimer;
-    private float enemySpawnTimerMax = 4f;
-
-    private void Start() {
-        enemySpawnTimer = 0;
-    }
+    private float spawnTimer = 0;
 
     private void Update() {
-        if (GameManager.Instance.IsPlayable()) {
-            if (transform.childCount < enemyCountMax) {
-                enemySpawnTimer += Time.deltaTime;
+        if (GameManager.Instance.IsPlayable() && transform.childCount < enemyCountMax) {
+            spawnTimer += Time.deltaTime;
 
-                if (enemySpawnTimer > enemySpawnTimerMax) {
-                    EntitySO enemyToSpawn = entityListSO.entitySOList[UnityEngine.Random.Range(0, entityListSO.entitySOList.Count)];
-                    SpawnEnemy(enemyToSpawn);
-                }
+            if (spawnTimer > spawnTimerMax) {
+                EntitySO enemyToSpawn = entityListSO.entitySOList[UnityEngine.Random.Range(0, entityListSO.entitySOList.Count)];
+                SpawnEnemy(enemyToSpawn);
             }
         }
     }
 
     private void SpawnEnemy(EntitySO enemyToSpawn) {
-        Vector2 direction = new Vector2(UnityEngine.Random.Range(-1f, 1f), UnityEngine.Random.Range(-1f, 1f)).normalized;
-        float distance = UnityEngine.Random.Range(0, spawnRadius);
-        Vector2 spawnPoint = (Vector2)transform.position + direction * distance;
+        Vector2 spawnPoint = new Vector2(
+            transform.position.x + UnityEngine.Random.Range(0, width) - width / 2,
+            transform.position.y + UnityEngine.Random.Range(0, height) - height / 2
+        );
 
         if (Enemy.SpawnEnemy(enemyToSpawn, spawnPoint, transform) != null) {
-            enemySpawnTimer = 0f;
+            spawnTimer = 0f;
         }
     }
 
     private void OnDrawGizmos() {
         // Draw radius of bounds
         Gizmos.color = gizmoColor;
-        Gizmos.DrawWireSphere(transform.position, spawnRadius);
+        Gizmos.DrawWireCube(transform.position, new Vector2(width, height));
     }
 }
